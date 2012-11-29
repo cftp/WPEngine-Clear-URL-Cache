041d63b7c034cd2cb3bde4cd64fc5c8bd1e630e9
Simon Wheatley <simon@sweetinteraction.com>
2012-11-29 12:28:49 +0000

    Fix some erroneous code docs

--- class-clear-url-cache.php
+++ class-clear-url-cache.php
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /**
  *
- * @package WPEngine Clear URL Cache
+ * @package Clear URL Cache on WPEngine
  * @since 0.1
  */
 class CFTP_Clear_URL_Cache extends CFTP_Clear_URL_Cache_Plugin {
--- wpengine-clear-url-cache.php
+++ wpengine-clear-url-cache.php
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 /**
  * Main plugin information and requires.
  *
- * @package Twitter Watcher
+ * @package Clear URL Cache on WPEngine
  * @since 0.1
  * @copyright Copyright (C) Code for the People Ltd
  */
